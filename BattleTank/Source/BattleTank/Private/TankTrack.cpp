130a633edacc70f003e6e23360249f46ac251678
VRDESKTOP\James <auron_ff10@hotmail.co.uk>
2017-04-13 20:55:00 +0100

    Completing Manual Tank Movement

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -5,7 +5,6 @@
 
 void UTankTrack::SetThrottle(float Throttle)
 {
-	// TODO Clamp throttle value
 	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDriveForce;
 	auto ForceLocation = GetComponentLocation();
 	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
