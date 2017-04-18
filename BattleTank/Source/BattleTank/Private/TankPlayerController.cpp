4f0af5370a0c252f8996e8c1b43d4371fd3b2dde
VRDESKTOP\James <auron_ff10@hotmail.co.uk>
2017-04-18 20:54:11 +0100

    BlueprintImplementableEvent

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -1,12 +1,23 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright James Herd.
 
 #include "BattleTank.h"
 #include "Tank.h"
+#include "TankAimingComponent.h"
 #include "TankPlayerController.h"
 
 void ATankPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
+
+	auto AimingComponent = GetControlledTank()->FindComponentByClass<UTankAimingComponent>();
+	if (AimingComponent)
+	{
+		FoundAimingComponent(AimingComponent);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Player Controller can't find aiming component at begin play."));
+	}
 }
 
 void ATankPlayerController::Tick(float DeltaTime)
--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -1,14 +1,16 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright James Herd.
 
 #pragma once
 
 #include "GameFramework/PlayerController.h"
 #include "TankPlayerController.generated.h"
 
+//Forward declarations
 class ATank;
+class UTankAimingComponent;
 
 /**
- * 
+ * Responsible for taking player aim input.
  */
 UCLASS()
 class BATTLETANK_API ATankPlayerController : public APlayerController
@@ -23,6 +25,9 @@ public:
 protected:
 	UFUNCTION(BlueprintCallable, Category = "Setup")
 	ATank* GetControlledTank() const;
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Setup")
+	void FoundAimingComponent(UTankAimingComponent* AimCompRef);
 
 private:
 	//Has tank move barrel to aim at point where crosshair intersects world
